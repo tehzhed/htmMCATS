8b50d8f9cde275936cd94a9aca23a0915ad57ad0
io <disanzo@dis.uniroma1.it>
2015-08-06 16:45:54 +0200

    s

--- lib/tm.h
+++ lib/tm.h
@@ -240,7 +240,7 @@ tm_time_t last_tuning_time; \
 		    	printf ("%.2f\t",gsl_vector_get (x, i)); \
 		} \
 		predicted_throughput=0; \
-		for (i = 1; i <= N; i++) \
+		for (i = 1; i <= NUMBER_THREADS; i++) \
 			predicted_throughput+=gsl_matrix_get(Q,i-1, i)*gsl_vector_get(x,i); \
 		for (i = NUMBER_THREADS+1; i<= 2*NUMBER_THREADS-1; i++) \
 			predicted_throughput+=gsl_matrix_get(Q,i-NUMBER_THREADS, i)*gsl_vector_get(x,i); \
@@ -401,7 +401,7 @@ tm_time_t last_tuning_time; \
                                 break; \
                         } \
         } \
-        }
+	}
 
 
 # define TM_BEGIN(b) { \
