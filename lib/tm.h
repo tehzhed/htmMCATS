42b780be6673c55de42e9876e2171c16ee3c42cf
io <disanzo@dis.uniroma1.it>
2015-08-06 16:03:43 +0200

    2

--- lib/tm.h
+++ lib/tm.h
@@ -240,10 +240,15 @@ tm_time_t last_tuning_time; \
 		    	printf ("%.2f\t",gsl_vector_get (x, i)); \
 		} \
 		predicted_throughput=0; \
-		for (i = 1; i <= NUMBER_THREADS; i++) \
-			predicted_throughput+=(Q,i-1, i)*gsl_vector_get(x,i); \
-		for (i = NUMBER_THREADS+1; i<= 2*NUMBER_THREADS-1; i++) \
+		printf ("predicted_throughput"); \
+		for (i = 1; i <= NUMBER_THREADS; i++) { \
+			predicted_throughput+=gsl_matrix_get(Q,i-1, i)*gsl_vector_get(x,i); \
+			printf ("\nMatrix %.2f\tvector %.2f\t",(Q,i-1, i), gsl_vector_get(x,i)); \
+		} \
+		for (i = NUMBER_THREADS+1; i<= 2*NUMBER_THREADS-1; i++) { \
 			predicted_throughput+=gsl_matrix_get(Q,i-NUMBER_THREADS, i)*gsl_vector_get(x,i); \
+			printf ("\nMatrix %.2f\tvector %.2f\t",gsl_matrix_get(Q,i-NUMBER_THREADS, i),gsl_vector_get(x,i)); \
+		} \
 		gsl_permutation_free(p); \
 		gsl_vector_free(x); \
 }
