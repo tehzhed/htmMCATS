2632fcfa7e597bdb59e510178b544765f31308b2
Daniele Riccardelli <daniele.riccardelli@gmail.com>
2016-08-11 01:45:33 +0100

    remove print check

--- lib/tm.h
+++ lib/tm.h
@@ -151,7 +151,6 @@ typedef unsigned long tm_time_t;
 })
 
 #  define PRINT_STATS() { \
-		if (num_interval) { \
 		printf("==================INTERVAL STATS==================\n"); \
 		printf("id = %i\tinterval = %u\tquota = %u\tstalled = %i\tETA = %lu\n", myThreadId, num_interval, quota, stalled, TM_OVERALL_ETA()); \
 		printf("peak = %u\tcommits = %u\tactive count = %u\tthreads = %i\taborts = %lu\tlocks = %lu\n", peak, normalized_commits, active_count, NUMBER_THREADS, normalized_aborts, current_cycle_locks); \
@@ -165,7 +164,6 @@ typedef unsigned long tm_time_t;
 		} \
 		printf("Chart_data\t%u\t%u\t%lu\t%i\t%s\t%lu\t%lu\n", normalized_commits, active_count, TM_OVERALL_ETA(), NUMBER_THREADS, policy == PROBE ? "PROBE" : "THROTTLE", normalized_aborts, current_cycle_locks); \
 		printf("==================================================\n"); \
-		} \
 	}
 
 #  define PROBE_POLICY() { \
